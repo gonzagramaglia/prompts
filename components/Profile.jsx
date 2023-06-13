d2c301c8537d7ad8a7c274d2d7a155b4d0f5b33f
Gonza <gonzagramaglia@gmail.com>
2023-06-13 04:29:20 -0300

    Profile Page

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,7 +1,27 @@
+import PromptCard from "./PromptCard"
 
-const Profile = () => {
+
+const Profile = ({ name, description, data, handleEdit, handleDelete }) => {
   return (
-    <div>Profile</div>
+    <section className="w-full mt-32" >
+      <h1 className="head_text text-left" >
+        <span className="emerald_gradient">{name} Profile</span>
+      </h1>
+      <p className="desc text-left" >
+        {description}
+      </p>
+      <div className="mt-10 prompt_layout" >
+        {data.map( (post) => (
+            <PromptCard 
+              key={post._id}
+              post={post}
+              handleEdit={() => handleEdit && handleEdit(post) }
+              handleDelete={() => handleDelete && handleDelete(post) }
+            />
+          )
+        )}
+      </div>
+    </section>
   )
 }
 
